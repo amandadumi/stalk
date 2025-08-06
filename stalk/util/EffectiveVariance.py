6ad89d282e96e8d41e7bca628ec789efa6903f3c
Juha Tiihonen <juha.tiihonen@tuni.fi>
2025-08-06 10:29:35 +0300

    Improve EffectiveVariance model, add node factorization

--- stalk/util/EffectiveVariance.py
+++ stalk/util/EffectiveVariance.py
@@ -5,38 +5,98 @@ __author__ = "Juha Tiihonen"
 __email__ = "tiihonen@iki.fi"
 __license__ = "BSD-3-Clause"
 
+from numpy import isnan, nan, array, mean, isscalar
+
 
 class EffectiveVariance():
-    samples = None
-    errorbar = None
+    _errorbar_data = None
 
     def __init__(
         self,
-        samples,
-        errorbar
+        samples=None,
+        errorbar=None
     ):
-        assert samples > 0, 'The number of samples must be > 0.'
-        self.samples = samples
-        assert errorbar > 0, 'The initial errorbar must be > 0.'
-        self.errorbar = errorbar
+        self._errorbar_data = []
+        if isscalar(samples) and isscalar(errorbar):
+            self.add_errorbar_data(samples, errorbar)
+        elif hasattr(samples, '__iter__') and hasattr(errorbar, '__iter__'):
+            # Assume two lists were provided
+            for s, e in zip(samples, errorbar):
+                self.add_errorbar_data(s, e)
+            # end for
+        # else: do nothing
+        # end if
     # end def
 
-    def get_samples(self, errorbar):
-        assert errorbar > 0, 'The requested errorbar must be > 0.'
-        samples = self.samples * self.errorbar**2 * errorbar**-2
-        return max(1, int(samples))
+    def add_errorbar_data(self, samples, errorbar):
+        if samples > 0 and errorbar > 0:
+            self.errorbar_data.append([samples, errorbar])
+        else:
+            msg = 'Both the samples and errobar must be larger than 0, '
+            msg += f'provided: samples={samples}, errorbar={errorbar}'
+            raise ValueError(msg)
+        # end if
     # end def
 
-    def get_errorbar(self, samples):
-        assert samples > 0, 'The requested samples must be > 0'
-        return self.errorbar * (float(self.samples) / samples)**0.5
+    @property
+    def errorbar_data(self):
+        return self._errorbar_data
+    # end def
+
+    @property
+    def var_eff(self):
+        if len(self.errorbar_data) == 0:
+            var_eff = nan
+        elif len(self.errorbar_data) == 1:
+            var_eff = self.errorbar_data[0][0] * self.errorbar_data[0][1]**2
+        else:
+            data = array(self.errorbar_data).T
+            var_eff = mean(data[0] * data[1]**2)
+        # end if
+        return var_eff
+    # end
+
+    def get_samples(self, errorbar, nodes=1):
+        if errorbar <= 0:
+            raise ValueError(f'The provided errorbar must be > 0, provided: {errorbar}')
+        # end if
+        if nodes < 1:
+            raise ValueError(f'The number of nodes must be > 0, provided: {nodes}')
+        # end if
+        if isnan(self.var_eff):
+            return -1
+        else:
+            samples = self.var_eff * errorbar**-2
+            return max(1, int(samples / nodes))
+        # end if
+    # end def
+
+    def get_errorbar(self, samples, nodes=1):
+        if samples <= 0:
+            raise ValueError(f'The provided samples must be > 0, provided: {samples}')
+        # end if
+        if nodes < 1:
+            raise ValueError(f'The number of nodes must be > 0, provided: {nodes}')
+        # end if
+        return (nodes * self.var_eff / samples)**0.5
+    # end def
+
+    def __add__(self, other):
+        if not isinstance(other, EffectiveVariance):
+            raise TypeError('EffectiveVariance can only be added to EffectiveVariance')
+        # end if
+        new_samples = [data[0] for data in self.errorbar_data]
+        new_samples += [data[0] for data in other.errorbar_data]
+        new_errorbars = [data[1] for data in self.errorbar_data]
+        new_errorbars += [data[1] for data in other.errorbar_data]
+        return EffectiveVariance(new_samples, new_errorbars)
     # end def
 
     def __str__(self):
-        if self.errorbar is None or self.samples is None:
-            return "Effective variances not set."
+        if isnan(self.var_eff):
+            return "Effective variance is not set."
         else:
-            return f"Effective variance: {(self.errorbar * self.samples)**2}"
+            return f"Effective variance: {self.var_eff}"
         # end if
     # end def
 
--- tests/unit_tests/util/test_EffectiveVariance.py
+++ tests/unit_tests/util/test_EffectiveVariance.py
@@ -1,31 +1,86 @@
+#!/usr/bin/env python3
+
+__author__ = "Juha Tiihonen"
+__email__ = "tiihonen@iki.fi"
+__license__ = "BSD-3-Clause"
+
 from pytest import raises
+from numpy import isnan, array
 from stalk.util.EffectiveVariance import EffectiveVariance
 from stalk.util.util import match_to_tol
 
 
 def test_EffectiveVariance():
-    # Test nominal
-    samples = 6
-    errorbar = 5.0
-    ev = EffectiveVariance(samples, errorbar)
-    # Test that we recover original samples
-    assert ev.get_samples(errorbar) == samples
-    # Test that we recover original errorbar
-    assert ev.get_errorbar(samples) == errorbar
-    # Test that half the errorbar needs quadruple samples
-    assert ev.get_samples(errorbar / 2) == 4 * samples
-    # Test that twice the samples results in 1/sqrt(2) errorbar
-    assert match_to_tol(ev.get_errorbar(2 * samples), errorbar * 2**-0.5, 0.1)
-    # Test that samples is always > 0
-    assert ev.get_samples(1e10) == 1
 
-    # Test degraded inits
-    with raises(AssertionError):
-        EffectiveVariance(0, errorbar)
-    with raises(AssertionError):
-        EffectiveVariance(samples, 0)
-    with raises(TypeError):
-        EffectiveVariance(samples)
-    with raises(TypeError):
-        EffectiveVariance()
+    ev0 = EffectiveVariance()
+    with raises(ValueError):
+        # Cannot add zero samples
+        ev0.add_errorbar_data(0, 0.1)
+    # end with
+    with raises(ValueError):
+        # Cannot add zero errorbar
+        ev0.add_errorbar_data(0.1, 0)
+    # end with
+    assert isnan(ev0.var_eff)
+    assert ev0.get_samples(2.0) == -1
+    assert isnan(ev0.get_errorbar(2))
+    str(ev0)
+
+    # Test basic properties using scalar init
+    samples = 5
+    errorbar = 6.0
+    ev1 = EffectiveVariance(samples, errorbar)
+    # Test that we recover original samples
+    assert ev1.get_samples(errorbar) == samples
+    # Test that we recover original errorbar
+    assert ev1.get_errorbar(samples) == errorbar
+    # Test that half the errorbar needs quadruple samples
+    assert ev1.get_samples(errorbar / 2) == 4 * samples
+    # Test that twice the samples results in 1/sqrt(2) errorbar
+    assert match_to_tol(ev1.get_errorbar(2 * samples), errorbar * 2**-0.5, 0.1)
+    # Test that samples is always > 0
+    assert ev1.get_samples(1e10) == 1
+    # Test the effective variance
+    assert match_to_tol(ev1.var_eff, samples * errorbar**2)
+    str(ev1)
+
+    # Test array init
+    samples2 = [5, 6]
+    errorbar2 = [6.0, 4.0]
+    ref = array(samples2) * array(errorbar2)**2
+    ev2 = EffectiveVariance(samples2, errorbar2)
+    assert ev2.var_eff > min(ref)
+    assert ev2.var_eff < max(ref)
+
+    # Test addition of errorbar data
+    ev1.add_errorbar_data(6, 4.0)
+    assert match_to_tol(ev1.var_eff, ev2.var_eff)
+
+    # Test nodes
+    with raises(ValueError):
+        ev1.get_samples(0.1, nodes=0)
+    # end with
+    with raises(ValueError):
+        ev1.get_errorbar(5, nodes=0)
+    # end with
+    nodes = 5
+    samples = 15
+    errorbar = 0.4
+    s1 = ev2.get_samples(errorbar, nodes=1)
+    sn = ev2.get_samples(errorbar, nodes=nodes)
+    # nodes * samples must approximately match
+    assert abs(s1 - nodes * sn) < nodes
+    e1 = ev2.get_errorbar(errorbar, nodes=1)
+    en = ev2.get_errorbar(errorbar, nodes=nodes)
+    # errorbar / sqrt(nodes) must remain constant
+    assert match_to_tol(e1, nodes**-0.5 * en)
+
+    # Test addition
+    ev3 = EffectiveVariance(7, 3.0)
+    ev_add = ev2 + ev3
+    assert match_to_tol(ev_add.errorbar_data[:-1], ev2.errorbar_data)
+    assert match_to_tol(ev_add.errorbar_data[-1], ev3.errorbar_data[-1])
+    assert not match_to_tol(ev_add.var_eff, ev2.var_eff)
+    assert not match_to_tol(ev_add.var_eff, ev3.var_eff)
+
 # end def
